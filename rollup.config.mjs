c39417d159490661ce6841af7a6be7b5f28a6fa1
BigTrikerTom <ts@thomas-steger.com>
2026-01-20 00:18:42 +0100

    Pfad 'linuxifizieren'

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,5 +1,14 @@
 import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// __dirname Ersatz in ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Absoluter Pfad zur tsconfig.json mit Linux-Slashes
+const tsconfigPath = path.join(__dirname, 'tsconfig.json').replace(/\\/g, '/');
 
 export default {
   input: 'src/pixel-http-card.ts',
@@ -11,7 +20,7 @@ export default {
   plugins: [
     resolve(),
     typescript({
-      tsconfig: './tsconfig.json', // relativer Pfad ohne __dirname oder path.resolve
+      tsconfig: tsconfigPath,
       sourceMap: true,
       module: 'ESNext'
     })
